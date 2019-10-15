1ebfda9cf254edd25f359e4310e6c0724d204313
Monil Soni <monilsoni99@gmail.com>
2019-10-15 20:15:24 +0530

    Removed Java configs and added XML configs along with addition of a dependency

--- spring-security-jdbc/src/main/java/com/monisoni/netbanking/dao/BankUserDAOImpl.java
+++ spring-security-jdbc/src/main/java/com/monisoni/netbanking/dao/BankUserDAOImpl.java
@@ -2,44 +2,32 @@ package com.monisoni.netbanking.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.monisoni.netbanking.entity.BankUser;
-import com.monisoni.netbanking.entity.BankUserRole;
 
 @Repository
 public class BankUserDAOImpl implements BankUserDAO {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Transactional
 	@Override
 	public BankUser findByUserName(String username) {
-		
-		BankUser user = null;
-
-		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(BankUser.class)
-				.addAnnotatedClass(BankUserRole.class).buildSessionFactory();
 
 		// create a session
-		Session session = factory.getCurrentSession();
+		Session session = sessionFactory.getCurrentSession();
 
-		try {
+		@SuppressWarnings("unchecked")
+		Query<BankUser> query = session.createQuery("from BankUser where userName = :username ");
+		query.setParameter("username", username);
+		BankUser user = (BankUser) query.uniqueResult();
 
-			session.beginTransaction();
-				
-			@SuppressWarnings("unchecked")
-			Query<BankUser> query = session.createQuery("from BankUser where userName = :username ");
-			query.setParameter("username", username);
-			user = (BankUser) query.uniqueResult();
-			
-			System.out.println(">>> " + user.toString() + "USERNAME: " + username);
-			
-			session.getTransaction().commit();
-
-		} finally {
-			session.close();
-			factory.close();
-		}
+		System.out.println(">>> " + user.toString() + "USERNAME: " + username);
 
 		return user;
 	}
