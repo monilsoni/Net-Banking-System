107e66923bf5996ab4315f53b9a7901286da1496
Monil Soni <monilsoni99@gmail.com>
2019-11-14 14:18:05 +0530

    Glitch

--- Netbanking-Application/src/main/java/com/monisoni/netbanking/dao/TransactionDAO.java
+++ Netbanking-Application/src/main/java/com/monisoni/netbanking/dao/TransactionDAO.java
@@ -1,10 +1,17 @@
 package com.monisoni.netbanking.dao;
 
 import java.sql.Date;
+import java.util.List;
+
+import com.monisoni.netbanking.entity.Transaction;
+
+
 
 public interface TransactionDAO {
 
 	public void addTransaction(String from, String to, String amount, String details, Date date,
 									long currentBalanceFrom, long currentBalanceTo);
+	
+	public List<Transaction> getMiniStatement(String account);
 	
 }
